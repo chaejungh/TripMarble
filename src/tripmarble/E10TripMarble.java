6785fb95e3136500e08b935fbd173b1b93255915
chaejungho <chaejungh@gmail.com>
2025-08-01 11:07:37 +0900

    .idea 삭제

--- src/tripmarble/E10TripMarble.java
+++ src/tripmarble/E10TripMarble.java
@@ -6,36 +6,51 @@ import java.awt.*;
 import java.util.ArrayList;
 
 class Marble extends JFrame{
-    ArrayList<Block> blockArrayList = new ArrayList<>();
+    ArrayList<CityBlock> blockArrayList = new ArrayList<>();
     int FRAME_WIDTH = 1200;
 
 
-    class Block{
+    class CityBlock {
+        enum CityClass{
+            아시아, 유럽, 명승지, 대도시, 한국
+        }
         int x;
         int y;
         String cityName;
-        int width;
-        int height;
+        int width =200;
+        int height =125;
         Color color;
+        int buildingPrice;
+        boolean isSoldHouse;
+        boolean isSoldBuilding;
+        boolean isSoldHotel;
+        CityClass cityClass;
 
-        public Block(int x, int y,int width,int height) { //drawRect 그릴때
-            this.x = x;
-            this.y = y;
-            this.height = height;
-            this.width = width;
-        }
-
-        public Block(int x, int y, String cityName, int width, int height, Color color) {
+        public CityBlock(int x, int y, String cityName, int width, int height, CityClass cityClass) {
             this.x = x;
             this.y = y;
             this.cityName = cityName;
             this.width = width;
             this.height = height;
-            this.color = color;
-        }
+            switch (cityClass){
+                case 아시아:
+                    color = Color.RED;
+                    buildingPrice=5;
+                    break;
+                case 유럽 :
+                    color=Color.YELLOW;
+                    buildingPrice=10;
+                    break;
+                case 명승지:
+                    color = Color.GREEN;
+                    buildingPrice = 15;
+                    break;
+                case 대도시:
+                    color = Color.BLUE;
+                    buildingPrice = 20;
+                    break;
 
-        Block(){
-
+            }
         }
     }
     class MyCanvas extends Canvas{
@@ -44,7 +59,7 @@ class Marble extends JFrame{
         }
         @Override
         public void paint(Graphics g) {
-            for (Block b: blockArrayList){
+            for (CityBlock b: blockArrayList){
                 g.setColor(b.color);
                 g.drawRect(b.x,b.y,b.width,b.height);
                 g.drawString(b.cityName,b.x+25,b.y+25);
@@ -55,7 +70,7 @@ class Marble extends JFrame{
     public Marble(){
         super("TripMarble");
         canvas = new MyCanvas();
-        Block seoul = new Block(0,200,"Seoul",200,125,Color.BLACK);
+        CityBlock seoul = new CityBlock(0,200,"Seoul",200,125, CityBlock.CityClass.한국);
         blockArrayList.add(seoul);
         this.add(canvas);
 
